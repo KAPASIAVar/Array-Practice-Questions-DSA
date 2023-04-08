9f1f8869865f3fa66c6d73ebfe0da677dd29f738
KAPASIAVar <96992611+KAPASIAVar@users.noreply.github.com>
2023-04-08 18:20:40 +0530

    Update IsArmstrong.java

--- IsArmstrong.java
+++ IsArmstrong.java
@@ -1,3 +1,16 @@
+/*Take the following as input.
+
+A number
+Write a function which returns true if the number is an armstrong number and false otherwise, where Armstrong number is defined as follows.
+
+A positive integer of n digits is called an Armstrong number of order n (order is number of digits) if.
+
+abcd… = pow(a,n) + pow(b,n) + pow(c,n) + pow(d,n) + ….
+
+1634 is an Armstrong number as 1634 = 1^4 + 6^4 + 3^4 + 4^4
+
+371 is an Armstrong number as 371 = 3^3 + 7^3 + 1^3
+*/
 import java.util.*;
 public class Main {
 	public static boolean CheckArm(int n){
