3569e9129bed383f9f566689cf4efeab129679d6
VARUN KAPASIA <96992611+KAPASIAVar@users.noreply.github.com>
2023-07-14 22:20:17 +0530

    Create Peak_ele_sorted_array.cpp

---
+++ Peak_ele_sorted_array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int peak_element(int arr[],int n){
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]<arr[mid+1]){
+            low=mid;
+        }
+        else if(arr[mid]<arr[mid-1]){
+            high=mid;
+        }
+        else return arr[mid];
+    }
+    return -1;
+}
+int main(){
+    int arr[]={1,2,3,10,20,25,10,6,8};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<peak_element(arr,n);
+}
