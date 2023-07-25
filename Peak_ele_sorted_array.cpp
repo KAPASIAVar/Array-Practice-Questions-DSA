95e359506d27823f061d255143dbef4db5c76d76
VARUN KAPASIA <96992611+KAPASIAVar@users.noreply.github.com>
2023-07-25 21:55:36 +0530

    Peak_ele_mountain.cpp

--- Peak_ele_sorted_array.cpp
+++ Peak_ele_sorted_array.cpp
@@ -1,17 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int peak_element(int arr[],int n){
-    int low=0;
+   int low=0;
     int high=n-1;
     while(low<=high){
         int mid=(low+high)/2;
-        if(arr[mid]<arr[mid+1]){
-            low=mid;
-        }
-        else if(arr[mid]<arr[mid-1]){
-            high=mid;
-        }
-        else return arr[mid];
+        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])return mid;
+        else if(arr[mid]<arr[mid-1] && arr[mid]>arr[mid+1])high=mid;
+        else low=mid;
     }
     return -1;
 }
