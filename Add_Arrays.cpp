b31686946b3d546c388673fe0f79c9de3d1ff3c4
VARUN KAPASIA <96992611+KAPASIAVar@users.noreply.github.com>
2023-07-29 23:47:57 +0530

    Update Add_Arrays.cpp

--- Add_Arrays.cpp
+++ Add_Arrays.cpp
@@ -1,36 +1,42 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-void addArrays(int a[],int n,int b[],int m){
+void solve(int arr1[],int n,int arr2[],int m){
+    int carry=0;
+    vector<int>ans;
     int i=n-1;
     int j=m-1;
-    vector<int>ans;
-    int carry=0;
+    
     while(i>=0 && j>=0){
-        int sum=a[i--]+b[j--]+carry;
+        int sum=arr1[i]+arr2[j]+carry;
         carry=sum/10;
-        sum=sum%10;
-        ans.push_back(sum);
-
+        ans.push_back(sum%10);
+        i--;
+        j--;
+        
     }
     while(i>=0){
-        int sum=a[i--]+carry;
+        int sum=arr1[i]+carry;
         carry=sum/10;
-        sum=sum%10;
-        ans.push_back(sum);
-
+        ans.push_back(sum%10);
+        i--;
     }
     while(j>=0){
-        int sum=b[j--]+carry;
+        int sum=arr2[j]+carry;
         carry=sum/10;
-        sum=sum%10;
-        ans.push_back(sum);
+        ans.push_back(sum%10);
+        j--;
+    }
+    while(carry!=0)
+    {
+        int sum=carry;
+        carry=sum/10;
+        ans.push_back(sum%10);
     }
     reverse(ans.begin(),ans.end());
     for(int i:ans)cout<<i;
 }
-int main() {
-    int a[]={2,5,7};
-    int b[]={2,4};
-    addArrays(a,3,b,2);
-
+int main(){
+    int arr1[]={1,2,3};
+    int arr2[]={4,5};
+    solve(arr1,3,arr2,2);
 }
